77cf487dae58eb307eeec09b9169fd06e5666e60
acidicnic <nic4096@gmail.com>
2020-02-26 03:29:12 -0800

    setup set tests

--- set_test.py
+++ set_test.py
@@ -1,9 +1,48 @@
-from binarytree import BinarySearchTree, BinaryTreeNode
+from set import Set
 import unittest
 
+class SetTest(unittest.TestCase):
+    def test_init(self):
+        s = Set()
 
+    def test_init_with_list(self):
+        s = Set()
 
+    def test_init_with_list_of_strings(self):
+        s = Set()
 
+    def test_init_with_list_of_tuples(self):
+        s = Set()
+
+    def test_iter(self):
+        s = Set()
+
+    def test_size(self):
+        s = Set()
+
+    def test_contains(self):
+        s = Set()
+
+    def test_add(self):
+        s = Set()
+
+    def test_remove(self):
+        s = Set()
+
+    def test_union(self):
+        s = Set()
+
+    def test_intersection(self):
+        s = Set()
+
+    def test_difference(self):
+        s = Set()
+
+    def test_is_subset(self):
+        s = Set()
+
+    def test_clear(self):
+        s = Set()
 
 
 if __name__ == '__main__':
